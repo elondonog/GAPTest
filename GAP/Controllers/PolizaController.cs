76604e0a1577a45ab63065c6629f113547b19d59
Edward Londono <elondonog@yahoo.es>
2018-09-09 23:45:37 -0500

    Carga inicial

---
+++ GAP/Controllers/PolizaController.cs
@@ -0,0 +1,94 @@
+﻿using GAP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GAP.Controllers
+{
+    public class PolizaController : Controller
+    {
+        // GET: Poliza
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Poliza/Details/5
+        [Authorize]
+        public ActionResult Details()
+        {        
+            var Poliza = new Poliza { NombrePoliza = "Poliza Automovil", DescripcionPoliza="La poliza de automovil cubre descripcion", FechaInicio=DateTime.Now, NumeroMesesCobertura=24, Precio=1200000, ApplicationUserId=User.Identity.Name, idTipoCobertura= 1, idTipoRiesgo=2, idTipoEstado= 1};
+            return View(Poliza);        
+        }
+
+        // GET: Poliza/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Poliza/Create
+        [HttpPost]
+        [Authorize]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var Poliza = new Poliza { NombrePoliza = "Poliza Automovil", DescripcionPoliza = "La poliza de automovil cubre descripcion", FechaInicio = DateTime.Now, NumeroMesesCobertura = 24, Precio = 1200000, ApplicationUserId = User.Identity.Name, idTipoCobertura = 2, idTipoRiesgo = 2, idTipoEstado = 1 };
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Poliza/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Poliza/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Poliza/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Poliza/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ GAP/Models/Poliza.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GAP.Models
+{
+    public class Poliza
+    {
+        public int Id { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        [Required]
+        [Display(Name = "Cobertura")]
+        public int idTipoCobertura { get; set; }
+
+        [Required]
+        [Display(Name = "Riesgo")]
+        public int idTipoRiesgo { get; set; }
+
+        [Display(Name = "Estado")]
+        [Required]
+        public int idTipoEstado { get; set; }
+
+        [Display(Name ="Nombre de la Poliza")]
+        [Required]
+        public string NombrePoliza { get; set; }
+
+        [Display(Name = "Descripcion de la Poliza")]
+        [Required]
+        public string DescripcionPoliza { get; set; }
+
+        [Display (Name = "Fecha de inicio")]
+        [DataType(DataType.Date)]
+        [Required]
+        //[RegularExpression(@"^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$", ErrorMessage ="Formato como dd/MM/yyyy")]
+        public System.DateTime FechaInicio { get; set; }
+
+        [Display(Name ="Número de meses cubiertos")]
+        [Required]
+        public Nullable<int> NumeroMesesCobertura { get; set; }
+
+        [Display(Name ="Valor de la poliza")]
+        [DataType(DataType.Currency)]
+        [Required]
+        public Nullable<decimal> Precio { get; set; }
+        
+    }
+}
